3e7936a97b2d3525721ba0f73832c0cba449f5d8
Francisco <fcolarich@gmail.com>
2021-07-30 01:09:00 -0300

    Added wrapping around the edges to clouds

--- Assets/Scripts/CloudMovement.cs
+++ Assets/Scripts/CloudMovement.cs
@@ -1,23 +1,51 @@
+using System;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class CloudMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 0.05f;
-    [SerializeField] private RectTransform screenRect;
+    [SerializeField] private Camera camera;
     private RectTransform _thisRect;
+    private Bounds _canvasBounds;
     private Vector3 _originalPosition;
+    private WaitForSeconds _waitForSeconds = new WaitForSeconds(1);
+    
 
     void Start()
     {
         _thisRect = GetComponent<RectTransform>();
         _originalPosition = _thisRect.position;
+        StartCoroutine(CheckIfVisible());
     }
 
     private void Update()
     {
         _thisRect.position += _thisRect.right * (speed * Time.deltaTime);
     }
+
+
+    private IEnumerator CheckIfVisible()
+    {
+        float cameraPixelWidth = camera.pixelWidth / 2;
+        var rect = _thisRect.rect;
+
+
+        var comparison = speed > 0 ? new Func<bool>(() => _thisRect.localPosition.x + rect.xMin > cameraPixelWidth) : 
+            (() =>_thisRect.localPosition.x - _thisRect.rect.xMin < - cameraPixelWidth);
+        
+        while (true)
+        {
+            yield return _waitForSeconds;
+            if (comparison())
+            {
+                _thisRect.localPosition = new Vector3(speed>0? rect.xMin-cameraPixelWidth:cameraPixelWidth-rect.xMin,_originalPosition.y+Random.Range(-50,50),_originalPosition.z);
+            }
+        }
+    }
+    
+
 
     
 }
