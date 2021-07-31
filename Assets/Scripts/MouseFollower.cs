d0a3ef2ec593f509025cbe2fa29ee546b3c3a3e0
Francisco <fcolarich@gmail.com>
2021-07-31 02:30:50 -0300

    Changes to UI

--- Assets/Scripts/MouseFollower.cs
+++ Assets/Scripts/MouseFollower.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
 
 public class MouseFollower : MonoBehaviour
 {
     [SerializeField] private Camera camera;
-    
+    [SerializeField] private Texture2D mouseTexture;
+
+    private void Start()
+    {
+        Cursor.SetCursor(mouseTexture,Vector2.zero, CursorMode.Auto);
+    }
+
     private void Update()
     {
             var mousePosition = Input.mousePosition;
