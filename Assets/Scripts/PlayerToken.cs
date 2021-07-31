e6f0a45835510045b6ee8f16747254d375a45ddb
Francisco <fcolarich@gmail.com>
2021-07-30 23:55:33 -0300

    Added Audio

--- Assets/Scripts/PlayerToken.cs
+++ Assets/Scripts/PlayerToken.cs
@@ -7,14 +7,20 @@ public class PlayerToken : MonoBehaviour
     [SerializeField] private Sprite[] sprites;
     [SerializeField] private float speed = 0.1f;
     [SerializeField] private GameObject impactParticleObject;
+    [SerializeField] private AudioClip[] audios;
+    [SerializeField] private AudioClip[] impactAudios;
     
     private SpriteRenderer _spriteRenderer;
     private WaitForEndOfFrame _waitForEndOfFrame;
+    private AudioSource _audioSource;
 
     void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _spriteRenderer.sprite = sprites[Random.Range(0,sprites.Length)];
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = audios[Random.Range(0,audios.Length)];
+        _audioSource.Play();
     }
 
     public IEnumerator MoveToPosition(int position)
@@ -25,8 +31,9 @@ public class PlayerToken : MonoBehaviour
             yield return _waitForEndOfFrame;
         }
         impactParticleObject.SetActive(true);
+        _audioSource.clip = impactAudios[Random.Range(0,impactAudios.Length)];
+        _audioSource.Play();
         
-
     }
     
 
